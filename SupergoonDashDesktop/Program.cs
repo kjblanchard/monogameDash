0731eb04976bfb071b5773983e977c1678c9889a
Kevin Blanchard <blanchardkevinj@gmail.com>
2022-05-22 15:24:16 -0400

    Initial commit, fmod with cross platform project

---
+++ SupergoonDashDesktop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SupergoonDashCrossPlatform;
+
+namespace SupergoonDashDesktop
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
---
+++ SupergoonEngine/Sound/SoundSystem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using FMOD.Studio;
+using Microsoft.Xna.Framework;
+
+namespace SupergoonDashCrossPlatform.Sound;
+
+public class SoundSystem : GameComponent
+{
+    #region Configuration
+
+    private FMOD.Studio.System _fmodStudioSystem;
+
+    public SoundSystem(Game game) : base(game)
+    {
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        InitializeFmodStudio();
+    }
+
+    /// <summary>
+    /// Creates the fmod studio system, and initializes the system
+    /// </summary>
+    private void InitializeFmodStudio()
+    {
+        FMOD.Studio.System.create(out _fmodStudioSystem);
+        _fmodStudioSystem.initialize(512, INITFLAGS.NORMAL, FMOD.INITFLAGS.NORMAL, IntPtr.Zero);
+
+        //load the bank files.
+        //Currently using a null out as I am not accessing the banks after loading them into the sound system.
+        _fmodStudioSystem.loadBankFile("Content/Sounds/Desktop/Master.bank", LOAD_BANK_FLAGS.NORMAL, out _);
+        _fmodStudioSystem.loadBankFile("Content/Sounds/Desktop/Master.strings.bank", LOAD_BANK_FLAGS.NORMAL,
+            out _);
+        
+    }
+
+    public void PlayBgm()
+    {
+        //get an event from the bank into a event description
+        _fmodStudioSystem.getEvent("event:/level1", out var description);
+        //load the song into memory
+        description.loadSampleData();
+        //create a playable instance
+        description.createInstance(out var tempInstance);
+        //play the song
+        tempInstance.start();
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+        _fmodStudioSystem.update();
+    }
+
+    #endregion
+}
